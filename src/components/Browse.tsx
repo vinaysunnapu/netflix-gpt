2df99ce24cdf0a7100cb60f22be65dcb094009dd
sunnapuvinay <138471157+sunnapuvinay@users.noreply.github.com>
2026-03-16 15:31:03 +0530

    created useNowPlayingMovies hook for api call

--- src/components/Browse.tsx
+++ src/components/Browse.tsx
@@ -1,6 +1,10 @@
 import Header from "./Header";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 
 const Browse = () => {
+
+  useNowPlayingMovies();
+  
   return (
     <div>
       <Header />
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,12 @@ export const LOGO =
 
 export const USER_AVATAR =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
+
+  export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOWI2N2E4N2NhNDVmMmJjMmNkMzEyMzcyYjlhNzc5YSIsIm5iZiI6MTc3MzY0MjY4MC43ODcwMDAyLCJzdWIiOiI2OWI3YTNiOGI3NDUwYTFmYjYwMWZkOTEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.mw9CBQvnsjITuA4tw1WCp6CxQl5Ph0W-ujRf2d4G6pU",
+  },
+};

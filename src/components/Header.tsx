1153c57f701295d9a4a93703c8660f7e5cf0e1ec
sunnapuvinay <138471157+sunnapuvinay@users.noreply.github.com>
2026-03-20 21:34:43 +0530

    made header fixed at the top

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -60,7 +60,7 @@ const Header = () => {
   };
 
   return (
-    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
+    <div className="fixed top-0 w-screen px-8 py-2 bg-gradient-to-b from-black z-40 flex flex-col md:flex-row justify-between">
       <img className="w-44 mx-auto md:mx-0" src={LOGO} alt="logo" />
       {user && (
         <div className="flex p-2 justify-between">
